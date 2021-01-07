e1e3a01468b46dbd4773883cbb531aca2f315388
richardreynaldo <richardreynaldo626@gmail.com>
2021-01-07 12:10:48 +0700

    Initial Commit

---
+++ KangSayurSkripsiVenus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KangSayurSkripsiVenus
+//
+//  Created by Laurentius Richard Reynaldo on 07/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KangSayurSkripsiVenus/KangSayurSkripsiVenusApp.swift
@@ -0,0 +1,17 @@
+//
+//  KangSayurSkripsiVenusApp.swift
+//  KangSayurSkripsiVenus
+//
+//  Created by Laurentius Richard Reynaldo on 07/01/21.
+//
+
+import SwiftUI
+
+@main
+struct KangSayurSkripsiVenusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
