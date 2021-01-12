f2c6dc8d05b15057686101333078e9e48f4337f9
richardreynaldo <richardreynaldo626@gmail.com>
2021-01-12 15:26:48 +0700

    gambar sampah + stepper

--- KangSayurSkripsiVenus/Keranjang/KeranjangRow.swift
+++ KangSayurSkripsiVenus/Keranjang/KeranjangRow.swift
@@ -11,46 +11,64 @@ struct KeranjangRow: View {
     var nama: String
     var harga: Int
     var gambar:String
+    @State var jumlah : Int = 1
     var body: some View {
-
         ZStack {
             RoundedRectangle(cornerRadius: 12, style: .continuous)
                 .fill(Color.white)
-            
-            HStack {
-                CheckView()
-                
-                // ini harusnya ada if buat check barangnya apa terus keluarin gambarnya berdasarkan barang
-                
-                Image(gambar)
-                    .resizable()
-                    .frame(width: 60, height: 60, alignment: .center)
-                
-                VStack(alignment: .leading) {
-                    Text("\(nama)")
-//                        .font(StyleFont.captionSmall)
-                        .fontWeight(.light)
-//                        .foregroundColor(StyleColors.accountPageCaptionSmall)
-                        .padding(.top, 10)
+            VStack{
+                HStack {
+                    CheckView()
                     
-                    Text("Rp.\(harga)")
-//                        .font(StyleFont.heading2)
-//                        .foregroundColor(StyleColors.accountPageLargeTitleH1H2Text)
+                    // ini harusnya ada if buat check barangnya apa terus keluarin gambarnya berdasarkan barang
                     
-                    //kurang yang tempat sampah buat delete sama + - buat quantity
+                    Image(gambar)
+                        .resizable()
+                        .frame(width: 60, height: 60, alignment: .center)
+                    
+                    VStack(alignment: .leading) {
+                        Text("\(nama)")
+    //                        .font(StyleFont.captionSmall)
+                            .fontWeight(.light)
+    //                        .foregroundColor(StyleColors.accountPageCaptionSmall)
+                            .padding(.top, 10)
+                        
+                        Text("Rp.\(harga)")
+    //                        .font(StyleFont.heading2)
+    //                        .foregroundColor(StyleColors.accountPageLargeTitleH1H2Text)
+                        
+                        //kurang yang tempat sampah buat delete sama + - buat quantity
+                    }
+                    .padding(.leading, 14)
+                    
+                     Spacer()
                 }
-                .padding(.leading, 14)
-                
-                 Spacer()
+                .padding(.horizontal, 22)
+                HStack
+                {
+                    Spacer()
+                    
+                    Button(action: {
+                        print("asu")
+                    }, label: {
+                        Image("trash")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                    })
+                    
+                    
+                    Stepper("\(jumlah)", value: $jumlah)
+                        .frame(width: 120, height: 20)
+                }
             }
-            .padding(.horizontal, 22)
+            
         }
     }
 }
 
 struct KeranjangRow_Previews: PreviewProvider {
     static var previews: some View {
-        KeranjangRow(nama: "Kangkung", harga: 20000, gambar: "")
+        KeranjangRow(nama: "Kangkung", harga: 20000, gambar: "", jumlah: 0)
     }
 }
 
