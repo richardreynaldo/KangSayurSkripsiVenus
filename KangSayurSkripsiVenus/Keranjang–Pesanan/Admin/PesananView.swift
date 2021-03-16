bc91f6435a9b3feda8d30d9cb7828731cc2abb1e
Rayhan Faluda <rayhanfaluda@gmail.com>
2021-03-16 19:25:56 +0700

    Update PesananView.swift
    Performance optimizations

--- KangSayurSkripsiVenus/Keranjang–Pesanan/Admin/PesananView.swift
+++ KangSayurSkripsiVenus/Keranjang–Pesanan/Admin/PesananView.swift
@@ -19,43 +19,23 @@ struct PesananView: View {
         UITableViewCell.appearance().backgroundColor = UIColor(StyleColors.secondaryYellow)
     }
     
-    var body: some View {
-        /* ZStack {
-            List {
-                ForEach(Array(stride(from: 1, to: 13, by: 1)).reversed(), id:\.self) { month in
-                    if (!historyData.history.filter({calendar.component(.month, from: $0.dateTime) ==  month && calendar.component(.year, from: $0.dateTime) ==  2021}).isEmpty) {
-                        Section(header: Text(String(calendar.monthSymbols[month-1]) + " " + String(year))) {
-                            ForEach(historyData.history.filter({calendar.component(.month, from: $0.dateTime) ==  month && calendar.component(.year, from: $0.dateTime) ==  2021}), id:\.id) { history in
-                                ZStack {
-                                    PesananRow(history: history)
-                                    
-                                    NavigationLink(destination: PesananDetailView(history: history)) {
-                                        EmptyView()
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-                .listRowBackground(StyleColors.secondaryYellow)
-            }
-            .listStyle(DefaultListStyle())
-        } */
-        
+    var body: some View {       
         ZStack {
             ScrollView {
                 LazyVStack {
-                    ForEach(Array(stride(from: 1, to: 13, by: 1)).reversed(), id:\.self) { month in
-                        if (!historyData.history.filter({calendar.component(.month, from: $0.dateTime) ==  month && calendar.component(.year, from: $0.dateTime) ==  2021}).isEmpty) {
-                            VStack {
-                                Text(String(calendar.monthSymbols[month-1]) + " " + String(year))
-                                    .font(Font.custom("Sora-SemiBold", size: 15))
-                                    .foregroundColor(StyleColors.titleText)
-                                
-                                ForEach(historyData.history.filter({calendar.component(.month, from: $0.dateTime) ==  month && calendar.component(.year, from: $0.dateTime) ==  2021}), id:\.id) { history in
-                                    if history.status == false {
-                                        NavigationLink(destination: PesananDetailView(history: history)) {
-                                            PesananRow(history: history)
+                    ForEach(Array(stride(from: 2020, to: 2025, by: 1)).reversed(), id:\.self) { year in
+                        ForEach(Array(stride(from: 1, to: 13, by: 1)).reversed(), id:\.self) { month in
+                            if (!historyData.history.filter({calendar.component(.month, from: $0.dateTime) ==  month && calendar.component(.year, from: $0.dateTime) ==  year}).isEmpty) {
+                                VStack {
+                                    Text(String(calendar.monthSymbols[month-1]) + " " + String(year))
+                                        .font(Font.custom("Sora-SemiBold", size: 15))
+                                        .foregroundColor(StyleColors.titleText)
+                                    
+                                    ForEach(historyData.history.filter({calendar.component(.month, from: $0.dateTime) ==  month && calendar.component(.year, from: $0.dateTime) ==  year}), id:\.id) { history in
+                                        if history.status == false {
+                                            NavigationLink(destination: PesananDetailView(history: history)) {
+                                                PesananRow(history: history)
+                                            }
                                         }
                                     }
                                 }
